a51bda918cdd091c22eef547691ca1daa7ca5b6b
israel_nzila <dynamoflash@gmail.com>
2024-02-01 15:02:46 +0200

    retweet

--- src/assets/myComponentTwitter/Tweet.jsx
+++ src/assets/myComponentTwitter/Tweet.jsx
@@ -11,19 +11,34 @@ import { useState } from "react";
 export default function Tweet({ tweet }) {
 
     const [state, setState] = useState(false)
+    const [nombre, setNombre] = useState(0)
+    const [repost, setRepost] = useState(false)
+    const [repostnomber, setRepostnomber] = useState(0)
 
+   function clickRepost(){
+    setRepost(!repost)
+    setRepostnomber(repostnomber + 1)
+
+
+   }
+
+
+  
+
+
+   const RepostIcons = repost ? BiRepost : BiRepost
+
+
+   
+   
     function handleClick() {
 
         setState(!state)
+        setNombre(nombre + 1)
 
     }
+
     const LikeIcons = state ? IoHeart : FaRegHeart
-
-
-//    < StateRepost/>
-
-
-
 
 
 
@@ -37,19 +52,20 @@ export default function Tweet({ tweet }) {
 
                 <div style={{ marginLeft: '8px', width: '98%' }}><p>{tweet.text}</p></div>
                 <img src={tweet.src} alt="" style={{ width: '98%' }} />
-
                 <div style={{ display: 'flex', gap: '60px', height: '25px', marginTop: '10px', }}>
                     <div style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
                         <Like src="project's pictures/Reply.svg"></Like>
                         <span>243</span>
                     </div>
-                    <div style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
-                        <repostIcon  color={state ? 'red' : ''}/>
-                        <span>16</span>
+                    <div onClick={clickRepost} style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
+                 
+                    {/* <RepostIcons /> */}
+                    <RepostIcons color={repost ? "#00ba7c" : ""} size={"25px"}/>
+                        <span>{repostnomber}</span>
                     </div>
-                    <div onClick={handleClick} style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
-                        <LikeIcons color={state ? 'white' : ''} />
-                        <span>34</span>
+                    <div onClick={handleClick}  style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
+                        <LikeIcons color={state ? 'white' : ''}/>
+                        <span> {nombre} </span>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'row', fontSize: '15px', gap: '15px' }}>
                         <Like src="project's pictures/Share.svg"></Like>
